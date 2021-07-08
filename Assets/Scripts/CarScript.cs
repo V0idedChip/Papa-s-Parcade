f06cfe553078d8c91c651fa59403e63c712b1ed5
Dylan <1022363@student.sae.edu.au>
2021-07-08 13:48:32 +0800

    Race Car game

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public Animator animator;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        animator.SetFloat("Horizontal", moveVelocity.x);
+        animator.SetFloat("Vertical", moveVelocity.y);
+        animator.SetFloat("Speed", moveVelocity.sqrMagnitude);
     }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+    }
+
+
 }
