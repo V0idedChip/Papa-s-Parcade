671d40ea1e9359a265e2545395c938af61a7dc26
Dylan <1022363@student.sae.edu.au>
2021-08-18 18:42:34 +0800

    Trying to fix things

--- Assets/Scripts/LarioController.cs
+++ Assets/Scripts/LarioController.cs
@@ -4,39 +4,24 @@ using UnityEngine;
 
 public class LarioController : MonoBehaviour
 {
-    public float moveSpeed;
-    public int health;
-    public Animator animator;
-    
+    private PLayerControlerScript pLayer;
     private Rigidbody2D rb;
-    private Vector2 moveVelocity;
 
-    void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-
+        pLayer = gameObject.GetComponent<PLayerControlerScript>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
-        Jump();
-    
-
-        
-        
-    }
-
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            float jumpVelocity = 1000f;
+            rb.velocity = Vector2.up * jumpVelocity;
         }
     }
 
-    
 
-    
+
 }
